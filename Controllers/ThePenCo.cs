c2bafa097d0b870c0fd5db1750597ba52f0995d1
W0825064 <W0825064@ADL-A1201I-26>
2023-10-13 14:54:14 -0400

    Add project files.

---
+++ Controllers/ThePenCo.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ThePenCo_.Controllers
+{
+    public class ThePenCo : Controller
+    {
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        /*public string Index()
+        {
+            return "This is my default action...";
+        }*/
+
+        // 
+        // GET: /HelloWorld/Welcome/ 
+
+        /*public string Welcome()
+        {
+            return "This is the Welcome action method...";
+        }*/
+        public string Welcome(string name, int ID = 1)
+        {
+            return HtmlEncoder.Default.Encode($"Hello {name}, ID: {ID}");
+        }
+    }
+}
